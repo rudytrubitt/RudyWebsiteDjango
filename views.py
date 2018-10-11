12c7410e58dad0c8d27626ec8ad76bd698baabe4
Rudy Trubitt <rtrubitt@lionel.com>
2018-10-10 18:18:34 -0700

    extend templates finished

--- views.py
+++ views.py
@@ -4,26 +4,25 @@ from django.shortcuts import render
 
 
 def index(request):
-    content_html_file = open("content/index.html","r")
-    content_html = content_html_file.read()
+    # content_html_file = open("content/index.html","r")
+    # content_html = content_html_file.read()
     context = {
         'title': 'Rudy Trubitt • Home',
-        'content': content_html,
+       
     }
-    return render(request, 'base.html', context)
+    return render(request, 'index.html', context)
 
 
 
 def contact(request):
     print('render contact page')
-    content_html_file = open("content/contact.html","r")
-    content_html = content_html_file.read()
-    print(content_html)
+    # content_html_file = open("content/contact.html","r")
+    # content_html = content_html_file.read()
+    #print(content_html)
     context = {
         'title': 'Contact',
-        'content': content_html,
     }
-    return render(request, 'base.html', context)
+    return render(request, 'contact.html', context)
 
 
 def github(request):
